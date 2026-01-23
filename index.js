30b61a265c3ff397a0328ab48ca561e2a3144fc9
Tyler Nguyen <tyjn@uw.edu>
2026-01-23 12:40:40 -0800

    add second chance and resume

--- index.js
+++ index.js
@@ -42,13 +42,44 @@ const cfsData = {
 const scData = {
   name: "Second Chance",
   goals: "The goal of Second Chance was to create a platform where users" +
-    " browse, buy, and sell secondhand clothing in an effort to reduce" +
+    " can browse, buy, and sell secondhand clothing in an effort to reduce" +
     " waste from practices such as fast fashion.",
-  role: "",
-  problem: "",
-  solution: "",
-  outcome: "",
-  takeaways: "",
+  role: "In this project, I worked  as a developer with a team to create the" +
+    " application. The division of work was done by feature page, with my" +
+    " responsiblity being the homepage and navigation bar that would be used" +
+    " throughout the site. I also handled troubleshooting of other members'" +
+    " pages when something went wrong. Most importantly I was the one" +
+    " responsible for setting up and integrating our firebase authentication" +
+    " and datastore into the application so that users could login and view" +
+    " others' posts.",
+  problem: "The problem inspiring the creation of Second Chance was the" +
+    " wasteful and unsustainable nature of industries like fast fashion," +
+    " which promote overconsumption with no solution for what to do with" +
+    " the clothes afterwards. Online marketplaces exist for selling used" +
+    " goods, however the number focusing specifically on secondhand clothing" +
+    " is limited, meaning users are forced to use a handful of specialized" +
+    " sites or filter through other products unrelated to clothing on general" +
+    " marketplaces.",
+  solution: "To help address this, our team worked to create a prototype site" +
+    " which focused specifically on the online resale of secondhand clothing." +
+    " We used Google Firebase as a free solution to host our website, as well" +
+    " as for key components such as secure authentication, and a cloud based" +
+    " datastore. We modeled the site after that of well known clothing brands" +
+    " to take advantage of their expertise in categorizing and to create a" +
+    " familiar experience for users.",
+  outcome: "While the final product was a bit rough around the edges and" +
+    " lacking key functionality necessary for a fully operable clothing" +
+    " clothing marketplace such as payment integration and actual shipping" +
+    " calculations, we successfully made a functional interface that could be" +
+    " used as a first iteration to add these features onto, as features" +
+    " such as basic search, filtering, and cart management are all there.",
+  takeaways: "This was my first real experience working on a development" +
+    " project with a team, and I think that the main thing I've learned from" +
+    " it is how to work collaboratively with others, despite varying" +
+    " skillsets and interests. From a technical perspective, working on" +
+    " Second Chance gave me experience integrating Firebase, which is" +
+    " something I hadn't worked with before, as well as dealing with a NoSQL" +
+    " datastore since I have primarily used RDBM systems before.",
 };
 const ccaData = {
   name: "",
